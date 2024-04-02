bcf20b5ad3a085c309449e51aa8a219c23b67b4f
Oskar Sundström <ossu2300@student.miun.se>
2024-04-02 04:42:37 +0200

    uppdaterat form så det kan läggas till kurs,  uppdaterat server, lägger till table, lägger till info i table, funkltion för att generera upp de som skrivs in i form till hemsidan., uppdaterat app.get

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 
 //Express och sql
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
-//Connect
+
+//Connect.
 const connection = mysql.createConnection({
     host: "localhost",
     user: "ogge",
@@ -26,17 +27,71 @@ connection.connect((err) => {
 //SQL
 
 
+connection.query("DROP TABLE IF EXISTS kurser;", (err, info) => {
+    if(err) throw err;
+    console.log("Table deleted " + info);
+});
 
 
+connection.query(`CREATE TABLE kurser (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    coursename VARCHAR(200),
+    coursecode VARCHAR(200),
+    syllabus VARCHAR(200),
+    progression VARCHAR(1))` , (err, table) => {
+        if(err) throw err;
+
+        console.log("Table created " + table);
+    });
+
+
+    //Insert nytt
+connection.query("INSERT INTO kurser(coursename, coursecode, syllabus, progression) VALUES (?, ?, ?, ?)", ["Backend-baserad webbutveckling", "DT207G", "https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT207G/", "B"], (err, results) => {
+    if (err) {
+        console.error("Failed insert " + err);
+        return;
+    }
+    console.table( results );
+});
 
 
 
 //---------------------------------------------------------------//
 const app = express();
+app.use(express.json());
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs"); //View engine ejs (html typ)
 app.use(express.static("public")); //Statiska filer (css typ)
+
+app.post("/addCourse", (request, response) => {
+    
+    const coursename = request.body.coursename;
+    const coursecode = request.body.coursecode;
+    const syllabus = request.body.syllabus;
+    const progression = request.body.progression;
+
+    
+   if (coursename != "" && coursecode != "" && syllabus != "" && progression != "") {
+    connection.query("INSERT INTO kurser(coursename, coursecode, syllabus, progression) VALUES (?, ?, ?, ?)", 
+    [coursename, coursecode, syllabus, progression], (err) => {
+        if (err) {
+            console.log("Failed insert " + err);
+            return; 
+        }
+        
+
+        app.get("/addCourse", (request, response) => {
+            connection.query("SELECT * FROM kurser", (rows) => {
+                response.render("addCourse", {courses: rows});
+                console.log(connection.query)
+            });
+})})}});
+
+
+
 
 
 //Route
@@ -47,7 +102,14 @@ app.get("/", (request, response) => {
 });
 
 app.get("/addCourse", (request, response) => {
-    response.render("addCourse");
+    connection.query("SELECT * FROM kurser", (err, rows) => {
+        if (err) {
+            console.log("failed: " + err );
+        }
+        response.render("addCourse", {courses: rows});
+        console.log(rows);
+    });
+
 
 });
 
@@ -61,6 +123,4 @@ app.get("/about", (request, response) => {
 app.listen(port, () => {
     console.log("Server started on: " + port)
 });
-
-
 
